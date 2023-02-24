5bf572846f42a6535ec28966bb70099642542b60
ElvisAds <elvisprojetoss@gmail.com>
2023-02-24 18:35:37 -0300

    FetchType Eager/Lazy, @EntityGraph, FetchMode SELECT/SUBSELECT/JOIN, @Query e @Modify

--- course/src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,11 +1,18 @@
 package com.ead.course.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ead.course.models.ModuleModel;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID>{
 
+	@Modifying
+	@Query(value="select * from tb_modules where course_course_id = :courseId", nativeQuery = true)
+	List<ModuleModel> fondAllLmodulesIntoCourse(@Param("courseId") UUID courseId);
 }
