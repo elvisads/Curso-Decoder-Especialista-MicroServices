7d69984f754fe99e35179f027abda4b6fb0446e7
ElvisAds <elvisprojetoss@gmail.com>
2023-03-01 18:18:04 -0300

    Criar RESTful para Module com Validações

--- course/src/main/java/com/ead/course/services/ModuleService.java
+++ course/src/main/java/com/ead/course/services/ModuleService.java
@@ -1,8 +1,22 @@
 package com.ead.course.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import com.ead.course.models.ModuleModel;
 
 public interface ModuleService {
 	
-	void delete(ModuleModel model);
+    void delete(ModuleModel moduleModel);
+
+    ModuleModel save(ModuleModel moduleModel);
+
+    Optional<ModuleModel> findModuleIntoCourse(UUID courseId, UUID moduleId);
+
+    List<ModuleModel> findAllByCourse(UUID courseId);
+
+    Optional<ModuleModel> findById(UUID moduleId);
+
+//    Page<ModuleModel> findAllByCourse(Specification<ModuleModel> spec, Pageable pageable);
 }
